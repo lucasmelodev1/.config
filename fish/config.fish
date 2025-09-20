a0d1c43612f81f0126f4239778dad86fa6945fe8
Lucas Rodrigues <lucasgabrielmelorodrigues@gmail.com>
2025-09-19 23:15:17 -0300

    feat: fish shell and hyprland

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+# Aliases
+alias ls='exa --icons'
+alias cd='z'
+alias nvimg='nvim --listen /tmp/godot.pipe'
+alias godot='/home/lucas/Executables/Godot_v4.4.1-stable_linux.x86_64'
+
+# Initialize zoxide
+zoxide init fish | source
+
+# Automatically start tmux if not already inside it
+if type -q tmux
+    if status is-interactive
+        if test -z "$TMUX"
+            if not string match -r 'screen|tmux' $TERM
+                exec tmux
+            end
+        end
+    end
+end
+
+# Add Go bin to PATH
+set -Ux PATH $HOME/go/bin $PATH
+
+set fish_greeting
